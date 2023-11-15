58490c98d65805063540d86b0778cbecf9b7d326
AMR BOUYOUSSEF <bouyoussefamr@gmail.com>
2023-11-15 20:31:12 +0000

    mandatory

--- main.h
+++ main.h
@@ -12,14 +12,14 @@
 
 extern char **environ;
 
-/*string functions*/
+/*String functions*/
 int _strlen(const char *str);
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
 char *_strdup(char *str);
 
-/*display functions*/
+/*Display functions*/
 char **tokenn(char *str);
 int get(int status, char ***args);
 void print_env(char **env);
@@ -27,7 +27,7 @@ char *path(char *input, char *path);
 int exec_cmd(char *str, char *strr, char **stri, int error);
 int check_cmd(char **input, char *av, char *path_line, int error);
 
-/*print functions*/
+/*Print functions*/
 void print_number(int n);
 void print(char *input, char *cmd, int error);
 void free_arr(char **arr);
